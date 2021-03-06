36b1c5bb0206b2f480e1967dbade92619c311740
tg-coder <76582848+tg-coder@users.noreply.github.com>
2021-03-06 09:12:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+function getParagraph1()
+{
+    onclick="show_para";
+}
+var inputs = []
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("part_1" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("part_2" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("part_3" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("part_4" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("part_5" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("show_para" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para").innerHTML = inputs.join(". ")
+
+
+function getParagraph1()
+{
+    onclick="show_para2";
+}
+var inputs = []
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("para2part1" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para2").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("para2part2" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para2").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("para2part3" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para4").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("para2part5" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para2").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("para2part6" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para2").innerHTML = inputs.join(". ")
+for(var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById("show_para2" + i).value)
+}
+inputs.join(". ");
+document.getElementById("show_para2").innerHTML = inputs.join(". ")
